4efc60a60c9582aa416140ceec528c442df4a4db
Adrian D'Souza <dsouzaadrian24@gmail.com>
2020-09-30 15:06:13 -0400

    Error-handling and stream for tasks and subtasks

--- lib/src/auth/firestore.dart
+++ lib/src/auth/firestore.dart
@@ -9,17 +9,32 @@ class Firestore {
   }
 
   Stream<List<TodoModel>> overviewTodos(String uid) {
-    return _firestore.collection('Todo').snapshots().map<List<TodoModel>>(
-        (event) => event.docs
-            .map<TodoModel>((e) => TodoModel.fromJson(e.data()))
+    return _firestore
+        .collection('Todo')
+        .where('owner', isEqualTo: uid)
+        .snapshots()
+        .map<List<TodoModel>>((event) => event.docs
+            .map<TodoModel>((e) => TodoModel.fromJson(e.id, e.data()))
             .toList());
+  }
+
+  Stream<List<TodoSubTaskModel>> todoSubtasks(String todoId) {
+    return _firestore
+        .collection('Todo')
+        .doc(todoId)
+        .collection('SubTasks')
+        .snapshots()
+        .map<List<TodoSubTaskModel>>((e) => e.docs.map<TodoSubTaskModel>(
+            (st) => TodoSubTaskModel.fromMap(st.id, st.data())));
   }
 
   Future<bool> addTodo(TodoModel todo) async {
     try {
+      print('Writing [addTodo]');
       await _firestore.collection('Todo').doc().set(todo.toMap());
       return true;
     } catch (e) {
+      print('Error on $e');
       return false;
     }
   }
