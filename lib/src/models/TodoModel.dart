ad010d6460786ae9e98f880680e412ef4dc21c92
Adrian D'Souza <dsouzaadrian24@gmail.com>
2020-09-30 15:06:56 -0400

    Update the model data

--- lib/src/models/TodoModel.dart
+++ lib/src/models/TodoModel.dart
@@ -1,22 +1,34 @@
 class TodoModel {
+  final String uid;
   final String todo;
-  final List<String> comments;
-  final List<String> tags;
-  final String groupsTo;
-  final String belongsTo;
+  final String owner;
+  final bool completed;
 
-  TodoModel(
-      {this.todo, this.comments, this.tags, this.groupsTo, this.belongsTo});
+  TodoModel({this.uid, this.todo, this.owner, this.completed});
 
-  factory TodoModel.fromJson(Map<String, dynamic> data) {
-    return TodoModel();
+  factory TodoModel.fromJson(String uid, Map<String, dynamic> data) {
+    return TodoModel(
+      uid: uid,
+      owner: data['owner'],
+      todo: data['todo'],
+      completed: data['completed'] ?? false,
+    );
   }
 
-  Map toMap() => {
-    "todo": todo,
-    "comments": comments,
-    "tags": tags,
-    "groupsTo": groupsTo,
-    "belongsTo": belongsTo
-  };
+  Map<String, dynamic> toMap() =>
+      {"todo": todo, "owner": owner, "completed": completed};
+}
+
+class TodoSubTaskModel {
+  final String subTask;
+  final String belongsTodo;
+
+  TodoSubTaskModel({this.subTask, this.belongsTodo});
+
+  factory TodoSubTaskModel.fromMap(String todoId, Map<String, dynamic> data) {
+    print('SubTask Data: $data');
+    return TodoSubTaskModel(subTask: data['subTask'], belongsTodo: todoId);
+  }
+
+  Map<String, dynamic> toMap() => {"subTask": subTask};
 }
